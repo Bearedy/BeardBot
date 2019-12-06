1c3ebb626821b918a3ce7c46acc7e444fbc0709a
Jerry Conner <36777315+bearedy@users.noreply.github.com>
2019-12-05 22:47:28 -0500

    save point

--- BeardBot/BeardBot.py
+++ BeardBot/BeardBot.py
@@ -1,71 +1,156 @@
 import math
+import time
 
 from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
 
-from util.orientation import Orientation
-from util.vec import Vec3
+
+class Vector3:
+    def __init__(self, data):
+        self.data = data
+
+    def __sub__(self, value):
+        return Vector3([self.data[0] - value.data[0], self.data[1] - value.data[1], self.data[2] - value.data[2]])
+
+    def __mul__(self, value):
+        return (self.data[0] * value.data[0] + self.data[1] * value.data[1] + self.data[2] * value.data[2])
 
 
-class PythonExample(BaseAgent):
+class obj:
+    def __init__(self):
+        self.location = Vector3([0, 0, 0])
+        self.velocity = Vector3([0, 0, 0])
+        self.rotation = Vector3([0, 0, 0])
+        self.rvelocity = Vector3([0, 0, 0])
+
+        self.local_location = Vector3([0, 0, 0])
+        self.boost = 0
+
+
+class exampleATBA:
+    def __init__(self):
+        self.expired = False
+
+    def execute(self, agent):
+        target_location = agent.ball
+        target_speed = velocity2D(agent.ball) + (distance2D(agent.ball, agent.me) / 1.5)
+
+        return agent.exampleController(target_location, target_speed)
+
+
+class Gosling(BaseAgent):
 
     def initialize_agent(self):
-        # This runs once before the bot starts up
-        self.controller_state = SimpleControllerState()
+        self.me = obj()
+        self.ball = obj()
+        self.start = time.time()
 
-    def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
-        ball_location = Vec3(packet.game_ball.physics.location)
+        self.state = exampleATBA()
 
-        my_car = packet.game_cars[self.index]
-        car_location = Vec3(my_car.physics.location)
+    def get_output(self, game: GameTickPacket) -> SimpleControllerState:
+        controller_state = SimpleControllerState()
+        self.preprocess(game)
+        return self.state.execute(self)
 
-        car_to_ball = ball_location - car_location
+    def exampleController(self, target_object, target_speed):
+        location = target_object.local_location
+        controller_state = SimpleControllerState()
+        angle_to_ball = math.atan2(location.data[1], location.data[0])
 
-        # Find the direction of our car using the Orientation class
-        car_orientation = Orientation(my_car.physics.rotation)
-        car_direction = car_orientation.forward
-
-        steer_correction_radians = find_correction(car_direction, car_to_ball)
-
-        if steer_correction_radians > 0:
-            # Positive radians in the unit circle is a turn to the left.
-            turn = -1.0  # Negative value for a turn to the left.
-            action_display = "turn left"
+        current_speed = velocity2D(self.me)
+        # steering
+        if angle_to_ball > 0.1:
+            controller_state.steer = controller_state.yaw = 1
+        elif angle_to_ball < -0.1:
+            controller_state.steer = controller_state.yaw = -1
         else:
-            turn = 1.0
-            action_display = "turn right"
+            controller_state.steer = controller_state.yaw = 0
 
-        self.controller_state.throttle = 1.0
-        self.controller_state.steer = turn
+        # throttle
+        if target_speed > current_speed:
+            controller_state.throttle = 1.0
+            if target_speed > 1400 and self.start > 2.2 and current_speed < 2250:
+                controller_state.boost = True
+        elif target_speed < current_speed:
+            controller_state.throttle = 0
 
-        draw_debug(self.renderer, my_car, packet.game_ball, action_display)
+        # dodging
+        time_difference = time.time() - self.start
+        if time_difference > 2.2 and distance2D(target_object.location, self.me.location) > 1000 and abs(
+                angle_to_ball) < 1.3:
+            self.start = time.time()
+        elif time_difference <= 0.1:
+            controller_state.jump = True
+            controller_state.pitch = -1
+        elif time_difference >= 0.1 and time_difference <= 0.15:
+            controller_state.jump = False
+            controller_state.pitch = -1
+        elif time_difference > 0.15 and time_difference < 1:
+            controller_state.jump = True
+            controller_state.yaw = controller_state.steer
+            controller_state.pitch = -1
 
-        return self.controller_state
+        return controller_state
+
+    def preprocess(self, game):
+        self.me.location.data = [game.game_cars[self.index].physics.location.x,
+                                 game.game_cars[self.index].physics.location.y,
+                                 game.game_cars[self.index].physics.location.z]
+        self.me.velocity.data = [game.game_cars[self.index].physics.velocity.x,
+                                 game.game_cars[self.index].physics.velocity.y,
+                                 game.game_cars[self.index].physics.velocity.z]
+        self.me.rotation.data = [game.game_cars[self.index].physics.rotation.pitch,
+                                 game.game_cars[self.index].physics.rotation.yaw,
+                                 game.game_cars[self.index].physics.rotation.roll]
+        self.me.rvelocity.data = [game.game_cars[self.index].physics.angular_velocity.x,
+                                  game.game_cars[self.index].physics.angular_velocity.y,
+                                  game.game_cars[self.index].physics.angular_velocity.z]
+        self.me.matrix = rotator_to_matrix(self.me)
+        self.me.boost = game.game_cars[self.index].boost
+
+        self.ball.location.data = [game.game_ball.physics.location.x, game.game_ball.physics.location.y,
+                                   game.game_ball.physics.location.z]
+        self.ball.velocity.data = [game.game_ball.physics.velocity.x, game.game_ball.physics.velocity.y,
+                                   game.game_ball.physics.velocity.z]
+        self.ball.rotation.data = [game.game_ball.physics.rotation.pitch, game.game_ball.physics.rotation.yaw,
+                                   game.game_ball.physics.rotation.roll]
+        self.ball.rvelocity.data = [game.game_ball.physics.angular_velocity.x,
+                                    game.game_ball.physics.angular_velocity.y,
+                                    game.game_ball.physics.angular_velocity.z]
+
+        self.ball.local_location.data = to_local(self.ball, self.me)
 
 
-def find_correction(current: Vec3, ideal: Vec3) -> float:
-    # Finds the angle from current to ideal vector in the xy-plane. Angle will be between -pi and +pi.
-
-    # The in-game axes are left handed, so use -x
-    current_in_radians = math.atan2(current.y, -current.x)
-    ideal_in_radians = math.atan2(ideal.y, -ideal.x)
-
-    diff = ideal_in_radians - current_in_radians
-
-    # Make sure that diff is between -pi and +pi.
-    if abs(diff) > math.pi:
-        if diff < 0:
-            diff += 2 * math.pi
-        else:
-            diff -= 2 * math.pi
-
-    return diff
+def to_local(target_object, our_object):
+    x = (target_object.location - our_object.location) * our_object.matrix[0]
+    y = (target_object.location - our_object.location) * our_object.matrix[1]
+    z = (target_object.location - our_object.location) * our_object.matrix[2]
+    return [x, y, z]
 
 
-def draw_debug(renderer, car, ball, action_display):
-    renderer.begin_rendering()
-    # draw a line from the car to the ball
-    renderer.draw_line_3d(car.physics.location, ball.physics.location, renderer.white())
-    # print the action that the bot is taking
-    renderer.draw_string_3d(car.physics.location, 2, 2, action_display, renderer.white())
-    renderer.end_rendering()
+def rotator_to_matrix(our_object):
+    r = our_object.rotation.data
+    CR = math.cos(r[2])
+    SR = math.sin(r[2])
+    CP = math.cos(r[0])
+    SP = math.sin(r[0])
+    CY = math.cos(r[1])
+    SY = math.sin(r[1])
+
+    matrix = []
+    matrix.append(Vector3([CP * CY, CP * SY, SP]))
+    matrix.append(Vector3([CY * SP * SR - CR * SY, SY * SP * SR + CR * CY, -CP * SR]))
+    matrix.append(Vector3([-CR * CY * SP - SR * SY, -CR * SY * SP + SR * CY, CP * CR]))
+    return matrix
+
+
+def velocity2D(target_object):
+    return math.sqrt(target_object.velocity.data[0] ** 2 + target_object.velocity.data[1] ** 2)
+
+
+def distance2D(target_object, our_object):
+    if isinstance(target_object, Vector3):
+        difference = target_object - our_object
+    else:
+        difference = target_object.location - our_object.location
+    return math.sqrt(difference.data[0] ** 2 + difference.data[1] ** 2)
